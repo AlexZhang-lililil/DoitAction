c736abeef269b4b6e676adfd949204934c4c6b3b
Bowen Bai <bbb@student.unimelb.edu.au>
2018-08-12 17:26:22 +1000

    sliding menu page( bookmark,history,achievement are included ) update

--- app/src/main/java/com/example/orquoll/swen90014_2018_or_quoll/MenuActivity.java
+++ app/src/main/java/com/example/orquoll/swen90014_2018_or_quoll/MenuActivity.java
@@ -7,13 +7,21 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toolbar;
+
+
 
 public class MenuActivity extends AppCompatActivity {
 
     private Button btn_setting;
     private Button btn_menu ;
     private DrawerLayout dlo_menu;
+    private SearchView search;
+    private TextView txt_bookmark;
+    private TextView txt_history;
+    private TextView txt_achievement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,10 @@ public class MenuActivity extends AppCompatActivity {
         btn_menu = (Button) findViewById(R.id.btn_menu);
         btn_setting = (Button) findViewById(R.id.btn_setting);
         dlo_menu = (DrawerLayout) findViewById(R.id.dlo_menu) ;
+        search = (SearchView) findViewById(R.id.srh) ;
+        txt_achievement = (TextView) findViewById(R.id.txt_achievements);
+        txt_bookmark = (TextView) findViewById(R.id.txt_bookmarks);
+        txt_history = (TextView) findViewById(R.id.txt_suggestion_history);
 
 
         btn_menu.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +52,40 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });
+
+        setListeners();
+
+        search.setIconifiedByDefault(false);
+        search.setSubmitButtonEnabled(true);
+
     }
+
+    private void setListeners (){
+        Onclick onclick = new Onclick();
+        txt_history.setOnClickListener(onclick);
+        txt_bookmark.setOnClickListener(onclick);
+        txt_achievement.setOnClickListener(onclick);
+    }
+
+    private class Onclick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            switch (view.getId())
+            {
+                case R.id.txt_achievements:
+                    intent = new Intent(MenuActivity.this,MyAchievementActivity.class);
+                    break;
+                case R.id.txt_bookmarks:
+                    intent = new Intent(MenuActivity.this,BookmarksActivity.class);
+                    break;
+                case R.id.txt_suggestion_history:
+                    intent = new Intent(MenuActivity.this,SuggestionHistoryActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    }
+
 }
