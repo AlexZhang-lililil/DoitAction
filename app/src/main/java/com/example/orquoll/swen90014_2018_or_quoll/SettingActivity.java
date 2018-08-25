232a080964822e257c89886c8c408ab0070c11c5
He Zhu <hzhu5@student.unimelb.edu.au>
2018-08-25 14:25:58 +1000

    xx

--- app/src/main/java/com/example/orquoll/swen90014_2018_or_quoll/SettingActivity.java
+++ app/src/main/java/com/example/orquoll/swen90014_2018_or_quoll/SettingActivity.java
@@ -3,7 +3,9 @@ package com.example.orquoll.swen90014_2018_or_quoll;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toolbar;
 
 public class SettingActivity extends AppCompatActivity {
@@ -18,6 +20,7 @@ public class SettingActivity extends AppCompatActivity {
 
         tb_setting = findViewById(R.id.tb_setting);
         btn_back = findViewById(R.id.btn_back_setting);
+        Spinner spinner = super.findViewById(R.id.spinnerNotification);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +29,9 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
 
-
+        String[] frequency = new String[]{"Three","One","Two","Five","Ten"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,frequency);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
